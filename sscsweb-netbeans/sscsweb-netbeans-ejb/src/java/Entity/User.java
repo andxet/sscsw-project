bbb78d2a707756122850067a09413c1c19663129
Andrea Peretti <and.xet@gmail.com>
2016-08-31 16:48:17 +0200

    Created User entity. Need testing

---
+++ sscsweb-netbeans/sscsweb-netbeans-ejb/src/java/Entity/User.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author andrea
+ */
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String nome;
+    
+    private String cognome;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date data_nascita;
+
+    private String username;
+
+    private String password;
+
+    private String facebook_token;
+    
+    private String google_auth_token;
+    
+    private String citta;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public Date getData_nascita() {
+        return data_nascita;
+    }
+
+    public void setData_nascita(Date data_nascita) {
+        this.data_nascita = data_nascita;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFacebook_token() {
+        return facebook_token;
+    }
+
+    public void setFacebook_token(String facebook_token) {
+        this.facebook_token = facebook_token;
+    }
+
+    public String getGoogle_auth_token() {
+        return google_auth_token;
+    }
+
+    public void setGoogle_auth_token(String google_auth_token) {
+        this.google_auth_token = google_auth_token;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ejb.Prova[ id=" + id + " ]";
+    }
+    
+}
